9bc0c648a44bd60d270077fb70eb6b16d3bf20ee
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2022-12-10 14:07:28 +0200

    DZ-1

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-const express = require('express');
+const fs = require('node:fs');
+const path = require('node:path');
 
-const app = express();
+const pathGirlsFile = path.join(__dirname, 'girls');
+const pathBoysFile = path.join(__dirname, 'boys');
 
-console.log(22);
+const sorter = ( directory, gender, pathGender ) => {
+    const pathName = path.join(__dirname, directory);
+
+    fs.readdir(pathName, ( err, data ) => {
+        if ( err ) {
+            console.log(err);
+            return;
+        }
+
+        data.forEach(file => {
+            const pathFile = path.join(pathName, file);
+
+            fs.readFile(pathFile, ( err, data ) => {
+                if ( err ) {
+                    console.log(err);
+                    return;
+                }
+
+                const value = JSON.parse(data);
+
+                if ( value.gender === gender ) {
+                    fs.rename(pathFile, path.join(pathGender, file), err => {
+                        console.log(err);
+                    });
+                }
+            });
+        });
+    });
+}
+
+sorter('boys', 'female', pathGirlsFile);
+sorter('girls', 'male', pathBoysFile);
